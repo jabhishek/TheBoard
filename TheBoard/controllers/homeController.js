e61ac52459f3a45db3b4e14fb4904d462284edf3
jain208 <abhi2000@gmail.com>
2014-04-15 17:02:41 +0100

    adding a data service

--- TheBoard/controllers/homeController.js
+++ TheBoard/controllers/homeController.js
@@ -1,7 +1,10 @@
-﻿(function (homeController) {
+﻿    (function (homeController) {
+        var data = require("../data");
     homeController.init = function(app) {
-        app.get("/", function (req, res) {
-            res.render("index", { title: "Express + vash" });
+        app.get("/", function(req, res) {
+            data.getNoteCategories(function(error, results) {
+                res.render("index", { title: "Express + vash", error: error, categories: results });
+            });
         });
     };
 })(module.exports)
