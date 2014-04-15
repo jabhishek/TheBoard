97b819757c336f92dc4583e3c275c00db6f04c4d
jain208 <abhi2000@gmail.com>
2014-04-15 15:27:05 +0100

    Express Hello World

--- TheBoard/server.js
+++ TheBoard/server.js
@@ -1,6 +1,18 @@
 ﻿var http = require('http');
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(req, res) {
+    res.send("<h1>Hello World</h1>");
+});
+
+app.get("/api/users", function (req, res) {
+    res.set("Content-Type", "application/json");
+    res.send({name: "Abhi", age: 35});
+});
+
+var server = http.createServer(app);
+
 var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+server.listen(port);
