380943b5e9acc39f716fada6b1c2293d29ef4c33
jain208 <abhi2000@gmail.com>
2014-04-17 23:31:42 +0100

    Trying to commit again

---
+++ TheBoard/server.js
@@ -0,0 +1,14 @@
+﻿var http = require('http');
+var express = require("express");
+var app = express();
+var controllers = require("./controllers");
+controllers.init(app);
+app.set("view engine", "vash");
+
+// set the public static resource folder
+app.use(express.static(__dirname + "/public"));
+
+var server = http.createServer(app);
+
+var port = process.env.port || 1313;
+server.listen(port);
