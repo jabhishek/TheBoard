b701253bfa4c025bcc9025093628c4b74dd73aaf
jain208 <abhi2000@gmail.com>
2014-04-17 23:24:09 +0100

    Installing grunt and grunt-nodemon

--- TheBoard/controllers/homeController.js
+++ TheBoard/controllers/homeController.js
@@ -3,7 +3,7 @@
     homeController.init = function(app) {
         app.get("/", function(req, res) {
             data.getNoteCategories(function(error, results) {
-                res.render("index", { title: "Express + vash", error: error, categories: results });
+                res.render("index", { title: "The Board", error: error, categories: results });
             });
         });
     };
