d0b17cedca9bcecd7be0672ca858f14bd5362232
jain208 <abhi2000@gmail.com>
2014-04-23 11:31:20 +0100

    Done setup to connect to mongodb

---
+++ TheBoard/data/database.js
@@ -0,0 +1,26 @@
+﻿(function (database) {
+    var mongodb = require("mongodb");
+    // theBoard is the name of the database. 
+    // If mongoDb doesn't find the database, it will create it for you.
+    var mongoUrl = "mongodb://localhost:27017/theBoard";
+    var theDb = null;
+    database.getDb = function(next) {
+        if (!theDb) {
+            // connect to database
+            mongodb.MongoClient.connect(mongoUrl, function(err, db) {
+                if (err) {
+                    next(err, null);
+                } else {
+                    theDb = {
+                        db: db
+                    };
+                    next(null, theDb);
+                }
+            });
+        } else {
+            next(null, theDb);
+        }
+
+    }
+
+})(module.exports)
